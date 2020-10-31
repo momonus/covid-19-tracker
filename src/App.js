8edd4484c980671ebd2a706e25867378753ef4e6
momonus <53513564+momonus@users.noreply.github.com>
2020-10-31 19:35:53 +0700

    create menuitem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  MenuItem,
+  FormControl,
+  Select,
+} from '@material-ui/core';
 import './App.css';
 
 function App() {
-  return (
+  const[countries,setCountries] = useState([]);
+
+
+  useEffect(()=>{
+    const getContriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => ({
+            name:country.country,
+            value: country.countryInfo.iso2
+          }));
+
+          setCountries(countries);
+        });
+    };
+
+    getContriesData();
+  },[]);
+
+   return (
     <div className="App">
-      <h1>lests build a covid 19 tracker </h1>
+      <div className="app_header">
+        <h1> COVID-19 TRACKER </h1>
+        <FormControl className="app_dropdown">
+        <Select variant="outlined" value="abc">
+          {
+            countries.map(country =>(
+            <MenuItem value={country.value}>{country.name}</MenuItem>))
+          }
+        </Select>
+      </FormControl>
+     
+      
+       </div>
     </div>
   );
 }
 
 export default App;
+ 
