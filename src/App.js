a94d48abbb6616d90d7ad6b9f1404f93f2830860
momonus <53513564+momonus@users.noreply.github.com>
2020-11-01 19:41:53 +0700

    create boxs

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import {
   FormControl,
   Select,
 } from '@material-ui/core';
+import InfoBox from './InfoBox';
+import Map from './Map';
 import './App.css';
 
 function App() {
   const[countries,setCountries] = useState([]);
+  const[country,setCountry] = useState('worldwide');
 
 
   useEffect(()=>{
@@ -27,21 +30,35 @@ function App() {
     getContriesData();
   },[]);
 
+  const onCountryChang = (event) => {
+    const countryCode = event.target.value;
+
+    console.log("YOOO >>>>",countryCode);
+
+    setCountry(countryCode);
+  };
+
    return (
     <div className="App">
       <div className="app_header">
         <h1> COVID-19 TRACKER </h1>
         <FormControl className="app_dropdown">
-        <Select variant="outlined" value="abc">
-          {
-            countries.map(country =>(
-            <MenuItem value={country.value}>{country.name}</MenuItem>))
-          }
+        <Select variant="outlined" onChange={onCountryChang}value={country}>
+        <MenuItem value="worldwide">Worldwide</MenuItem>
+          {countries.map((country)=>(
+            <MenuItem value={country.value}>{country.name}</MenuItem>
+          ))}
         </Select>
       </FormControl>
-     
-      
        </div>
+       <div className="app_stats">
+         <InfoBox title="Coronavirus Cases" cases={123}total={2000}/>
+         <InfoBox title="Recovered"cases={1234}total={3000}/>
+         <InfoBox title="Deaths"cases={12345}total={4000}/>
+       </div>
+       <Map>
+         
+       </Map>
     </div>
   );
 }
