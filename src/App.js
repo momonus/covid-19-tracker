80c242ffd3a6af56f6a3e6bb84f5ad0e4048338e
momonus <53513564+momonus@users.noreply.github.com>
2020-11-04 22:16:01 +0700

    creat Worldwide new

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import InfoBox from './InfoBox';
 import Map from './Map';
 import './App.css';
 import Table from './Table';
+import {sortData} from "./util";
+import LineGraph from './LineGraph'
 
 function App() {
   const[countries,setCountries] = useState([]);
@@ -35,7 +37,8 @@ function App() {
             name:country.country,
             value: country.countryInfo.iso2
           }));
-          setTableData(data); 
+          const sortedData = sortData(data);
+          setTableData(sortedData); 
           setCountries(countries);
         });
     };
@@ -85,6 +88,7 @@ function App() {
         <h2>Live Cases by Country</h2>
         <Table countries={tableData}/>
         <h2>Worldwide new</h2>
+        <LineGraph/>
       </CardContent>
       </Card>
     </div>
